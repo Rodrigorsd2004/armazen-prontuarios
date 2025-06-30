03bbfd4642cbbadb7a991c03ce600f77cc354e5a
Rodrigo Sartori <rodrigorochasartori1@gmail.com>
2025-06-30 08:44:05 -0300

    Melhorias nos forms, mask e uppercase

--- src/components/ModalProntuario.jsx
+++ src/components/ModalProntuario.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { verificarSenha } from "../utils/verificaSenha";
 import ConfirmModal from "./ConfirmModal";
+import "./styles/ModalProntuario.css";
 
 const modeloExcel = "/modelo-prontuario.xlsx";
 
